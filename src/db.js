5f2625f37a864188f6bd3279ebb05868750c2eee
Nishant More <nishantmore228@gmail.com>
2024-08-24 15:31:46 +0530

    changes db env

--- src/db.js
+++ src/db.js
@@ -3,10 +3,14 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// export const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
+// for deployment
+const dbUrl = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`
+
+export const db = mysql.createConnection(dbUrl);
